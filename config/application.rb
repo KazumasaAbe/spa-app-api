9ca5899bdeea6bd05f49e81e0e0d18673c939156
iijima-tatsunori <himekawamaki45@gmail.com>
2021-07-16 09:35:07 +0900

    finished_model_initial_setting

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
     config.time_zone = ENV["TZ"]
