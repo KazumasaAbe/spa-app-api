599ad9057cbb6feaba7c73b4228002d795527054
iijima-tatsunori <himekawamaki45@gmail.com>
2021-07-22 14:15:40 +0900

    add_seed_data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-HostDetail.create!(name: "やおちゅう",
-                  latitude: "",
-                  longitude: "",
-                  acceptable_date: "",
-                  rate: "",
-                  description: "",
-                  link: "",
-                  address: "",
-                  acceptable: 1)
-
-HostDetail.create!(name: "空",
-                  latitude: "",
-                  longitude: "",
+HostDetail.create!(name: "遠野旅の産地直売所",
+                  latitude: "39.33159282407645",
+                  longitude: "141.53075750707873",
                   acceptable_date: "",
                   rate: "",
                   description: "",
                   link: "",
                   address: "",
                   acceptable: 0)
+  
+  HostDetail.create!(name: "やおちゅう",
+                  latitude: "39.33006788297404",
+                  longitude: "141.52967199975944",
+                  acceptable_date: "",
+                  rate: "2カッパ",
+                  description: "遠野でサイフォンコーヒーがあるのはここだけ。待っている間もコーヒーができるまでゆっくり楽しむことができます。",
+                  link: "https://tono-yamasatonet.com/tabisite/service01/%e3%82%84%e3%81%8a%e3%81%a1%e3%82%85%e3%81%86/",
+                  address: "〒028-0523 岩手県遠野市中央通り４−４５",
+                  acceptable: 1)
+
+HostDetail.create!(name: "遠野醸造TAPROOM",
+                  latitude: "39.33062506152027",
+                  longitude: "141.53040632658883",
+                  acceptable_date: "",
+                  rate: "",
+                  description: "旬のクラフトビールが数種類あります。まちぶらではこだわりあるクラフトビールとナッツを試食。旅の最後にまた立ち寄りたくなるスポットです。",
+                  link: "https://tono-yamasatonet.com/tabisite/service01/%e9%81%a0%e9%87%8e%e9%86%b8%e9%80%a0taproom/",
+                  address: "〒028-0523 岩手県遠野市中央通り１０−１５",
+                  acceptable: 1)
